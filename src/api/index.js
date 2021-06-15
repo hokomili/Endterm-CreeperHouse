1e35adc50dee5a3b5ac5a40f6cabb7bfe5944117
hokomili <28939939+hokomili@users.noreply.github.com>
2021-06-15 10:55:08 +0800

    firebase

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import jsonInfo from "../json/jsonInfo.json";
+import products from "../json/products.json";
+// Initialize the FirebaseUI Widget using Firebase.
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+firebase.initializeApp(firebaseConfig);
